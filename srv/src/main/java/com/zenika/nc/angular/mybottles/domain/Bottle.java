00ab443b7f3afe195694fcaffcf7e0a692cfe2bb
yoann <yoann.prot@afp.com>
2014-02-05 11:49:08 +0100

    Change image management on server side to simplify TP4

--- srv/src/main/java/com/zenika/nc/angular/mybottles/domain/Bottle.java
+++ srv/src/main/java/com/zenika/nc/angular/mybottles/domain/Bottle.java
@@ -1,69 +1,81 @@
 package com.zenika.nc.angular.mybottles.domain;
 
 public class Bottle {
-    private int id;
-    private String name;
-    private String description;
-    private int rate;
-    private String picture;
+  private int id;
+  private String name;
+  private String description;
+  private int rate;
+  private String picture;
+  private String contentType;
 
-    public Bottle() {}
+  public Bottle() {
+  }
 
-    public Bottle(String name, String description, int rate) {
-        this.name = name;
-        this.description = description;
-        this.rate = rate;
-        this.picture = null;
-    }
+  public Bottle(String name, String description, int rate) {
+    this.name = name;
+    this.description = description;
+    this.rate = rate;
+    this.picture = null;
+    this.picture = contentType;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public int getRate() {
-        return rate;
-    }
+  public int getRate() {
+    return rate;
+  }
 
-    public void setRate(int rate) {
-        this.rate = rate;
-    }
+  public void setRate(int rate) {
+    this.rate = rate;
+  }
 
-    public String getPicture() {
-        return picture;
-    }
+  public String getPicture() {
+    return picture;
+  }
 
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
+  public void setPicture(String picture) {
+    this.picture = picture;
+  }
 
-    @Override
-    public String toString() {
-        return "Bottle{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", rate=" + rate +
-                ", picture='" + picture + '\'' +
-                '}';
-    }
+  public String getContentType() {
+    return contentType;
+  }
+
+  public void setContentType(String contentType) {
+    this.contentType = contentType;
+  }
+
+  @Override
+  public String toString() {
+    return "Bottle{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", description='" + description + '\'' +
+            ", rate=" + rate +
+            ", picture='" + picture + '\'' +
+            ", contentType='" + contentType + '\'' +
+            '}';
+  }
 }
