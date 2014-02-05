61ee40331c0f0d34065bdca2c0d2d43a42c08fb3
yoann <yoann.prot@afp.com>
2014-02-05 11:58:36 +0100

    minor change to improve rest controller

--- ui/app/js/controller/restbottlecontroller.js
+++ ui/app/js/controller/restbottlecontroller.js
@@ -1,43 +1,46 @@
 angular.module('myBottles')
-.controller('BottleController', function BottleController($scope, $resource) {
-    // Create a new Resource object
-    var Bottle = $resource('/api/bottle/:bottleId', {bottleId:'@id'});
+    .controller('BottleController', function BottleController($scope, $resource) {
+        // Create a new Resource object
+        var Bottle = $resource('/api/bottle/:bottleId', {bottleId: '@id'});
 
-    // load bottle from REST interface with Resource object
-    $scope.bottles = Bottle.query();
+        // load bottle from REST interface with Resource object
+        $scope.bottles = Bottle.query();
 
-    $scope.newBottle = function () {
-        var bottle = {
-            name : '',
-            description: '',
-            rate : 1
-        }
-        // Create resource object from bottle
-        $scope.currentBottle = new Bottle(bottle);
-    };
+        $scope.newBottle = function () {
+            var bottle = {
+                name: '',
+                description: '',
+                rate: 1
+            }
+            // Create resource object from bottle
+            $scope.currentBottle = new Bottle(bottle);
+        };
 
-    $scope.createBottle = function () {
-        // Save current object
-        $scope.currentBottle.$save(function(bottle) {
-            $scope.currentBottle = bottle;
-            $scope.bottles = Bottle.query();
-        });
-    };
+        $scope.createBottle = function () {
+            // Save current object
+            $scope.currentBottle.$save(function (bottle) {
+                $scope.currentBottle = bottle;
+                //refresh bottles list
+                $scope.bottles = Bottle.query();
+                // clear view with a new bottle
+                $scope.newBottle();
+            });
+        };
 
-    $scope.changeCurrentBottle = function(id) {
-        // Display the bottle from id
-        $scope.currentBottle = Bottle.get({bottleId:id});
-    };
+        $scope.changeCurrentBottle = function (id) {
+            // Display the bottle from id
+            $scope.currentBottle = $scope.bottles[id];
+        };
 
-    $scope.delete = function() {
-        // Delete current object from server and update bottles list
-        $scope.currentBottle.$delete(function(){
-            //refresh bottles list
-            $scope.bottles = Bottle.query();
-            // clear view with a new bottle
-            $scope.newBottle();
-        });
-    };
+        $scope.delete = function () {
+            // Delete current object from server and update bottles list
+            $scope.currentBottle.$delete(function () {
+                //refresh bottles list
+                $scope.bottles = Bottle.query();
+                // clear view with a new bottle
+                $scope.newBottle();
+            });
+        };
 
-    $scope.newBottle();
-});
+        $scope.newBottle();
+    });
